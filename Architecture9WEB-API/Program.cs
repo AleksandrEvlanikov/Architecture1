be2588ee687b0c065634c7a09eed1f19ed8f4f4b
Aleksandr <aleksandr.evlanicov@gmail.com>
2023-12-25 18:47:30 +0300

    WEB-API

---
+++ Architecture9WEB-API/Program.cs
@@ -0,0 +1,37 @@
+
+using Architecture9WEB_API.Models;
+
+namespace Architecture9WEB_API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddSingleton<WeatherForecastHolder>();
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
